06ca477ef12b9e9493c02c5da11a37215075e1c6
neel-dabhi <yoursdabhineelkanth@gmail.com>
2025-05-13 19:07:34 -0300

    Completed Design 1

--- Sample.java
+++ Sample.java
@@ -1,7 +1,51 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
+// Time Complexity : O(N)
+// Space Complexity : O(N)
+// Did this code successfully run on Leetcode : YES
 // Any problem you faced while coding this :
 
+class MyHashSet {
 
-// Your code here along with comments explaining your approach
+    int ARRAY_SIZE = 100;
+    List<List<Integer>> hashList;
+
+    public MyHashSet() {
+        hashList = new ArrayList<>(ARRAY_SIZE);
+        for (int i = 0; i < ARRAY_SIZE; i++) {
+            hashList.add(null);
+        }
+    }
+
+    public void add(int key) {
+        int index = key % ARRAY_SIZE;
+        List<Integer> ll = hashList.get(index);
+
+        if( ll == null){
+            ll = new LinkedList<>();
+            ll.add(key);
+            hashList.set(index, ll);
+        }else{
+            if(!ll.contains(key)){
+                ll.add(key);
+            }
+        }
+    }
+
+    public void remove(int key) {
+        int index = key % ARRAY_SIZE;
+        List ll = hashList.get(index);
+
+        if (ll != null && ll.contains(key)) {
+            ll.remove(Integer.valueOf(key));
+        }
+    }
+
+    public boolean contains(int key) {
+        int index = key % ARRAY_SIZE;
+        List ll = hashList.get(index);
+
+        if (ll != null && ll.contains(key)) {
+            return true;
+        }
+        return false;
+    }
+}
